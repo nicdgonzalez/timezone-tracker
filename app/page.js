1fe2954766a7fd6d0aa95dd4f094cf7b6dfe4d71
Nicolas Gonzalez <ndgfrs@gmail.com>
2024-07-03 01:53:37 -0400

    Remove unused imports

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import styles from "./page.module.css";
 import { Header } from "./_components/Header";
-import { Card } from "./_components/Card";
 import { CardDisplay } from "./_components/CardDisplay";
 
 export default function Home() {
